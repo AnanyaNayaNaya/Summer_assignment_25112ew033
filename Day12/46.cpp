7c766277808165577c26117adda6737afda2d6e9
Ananya Shukla <theanjxshu@gmail.com>
2026-06-16 19:23:41 +0530

    Day12

---
+++ Day12/46.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+bool isArmstrong(int n)
+{
+    int temp = n, sum = 0, rem;
+
+    while (n > 0)
+    {
+        rem = n % 10;
+        sum += rem * rem * rem;
+        n /= 10;
+    }
+
+    return temp == sum;
+}
+
+int main()
+{
+    int num;
+    cin >> num;
+
+    if (isArmstrong(num))
+        cout << "Armstrong Number";
+    else
+        cout << "Not Armstrong Number";
+
+    return 0;
+}
