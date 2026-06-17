e15e008efaec434a3de7321a350c9e3305e36dde
Ananya Shukla <theanjxshu@gmail.com>
2026-06-17 19:22:54 +0530

    Day13

---
+++ Day13/52.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Enter number of elements: ";
+    cin >> n;
+
+    int arr[n];
+    int even = 0, odd = 0;
+
+    cout << "Enter elements:\n";
+    for(int i = 0; i < n; i++) {
+        cin >> arr[i];
+
+        if(arr[i] % 2 == 0)
+            even++;
+        else
+            odd++;
+    }
+
+    cout << "Number of Even elements = " << even << endl;
+    cout << "Number of Odd elements = " << odd << endl;
+
+    return 0;
+}
